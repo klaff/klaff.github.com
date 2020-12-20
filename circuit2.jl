072ff0da438fa9f404fe153cf5011ca8aa30ac0d
klaff <dave@klaff.org>
2020-12-20 13:38:08 -0600

    pushing schematic images

---
+++ circuit2.jl
@@ -0,0 +1,25 @@
+### A Pluto.jl notebook ###
+# v0.12.17
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ cddb9fd2-42f7-11eb-06f1-cb5b015b1b5e
+begin
+	using DifferentialEquations
+	using PlutoUI
+	using Plots
+end
+
+# ╔═╡ b5dbac40-42f7-11eb-1ea2-1d5824443de6
+md"""
+#### Let's simulate a more complex circuit
+"""
+
+# ╔═╡ baf6b640-42fa-11eb-3b37-c7e90b515fdf
+
+
+# ╔═╡ Cell order:
+# ╠═b5dbac40-42f7-11eb-1ea2-1d5824443de6
+# ╠═baf6b640-42fa-11eb-3b37-c7e90b515fdf
+# ╠═cddb9fd2-42f7-11eb-06f1-cb5b015b1b5e
